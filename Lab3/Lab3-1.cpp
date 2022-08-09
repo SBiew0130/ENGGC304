cc9b3e50bc373698807243218e2ce9b245d13eeb
biew <108038906+SBiew0130@users.noreply.github.com>
2022-08-09 23:19:31 +0700

    Add files via upload

---
+++ Lab3/Lab3-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+	int    a  = 5 ;					// %d
+	double b  = 12.23945 ;			// %f
+	char   c  = 'F' ;				// %c
+	
+	printf ( "%30d \n", a ) ;
+	printf ( "%30.2f \n", b ) ;
+ 	printf ( "%30c \n", c ) ;
+ 	
+	return 0 ;
+	
+}//end function
---
+++ Lab3/Lab3-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+	int    a  = 9 ;					// %d
+	double b  = 13.1 ;				// %f
+	char   c  = 'M' ;				// %c
+	
+	printf ( "%30d \n", a ) ;
+	printf ( "%30.2f \n", b ) ;
+ 	printf ( "%30c \n", c ) ;
+ 	
+	return 0 ;
+	
+}//end function
