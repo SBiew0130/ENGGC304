c565e2dcf8580d423f5e237c4a9633d9e064b4d7
biew <108038906+SBiew0130@users.noreply.github.com>
2022-10-09 23:44:58 +0700

    Add files via upload

---
+++ Laba12/Lab12.cpp
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<string.h>
+
+struct employeedata {
+	char name[100] ;
+	float salary ; 
+	int duration ; 
+} typedef worker ;
+
+void control( char incworker[]) ; 
+void inputdata( worker employ[], int workernumber ) ; 
+void showallsalary( worker employ[], int workernumber ) ;
+
+int main() {
+	char incworker[2] = "y"; 
+	int workernumber = 0 ;
+	worker employ[100] ;
+	while( incworker[0] == 'y') {
+		control( incworker) ; 
+		if (incworker[0] == 'y') { 
+            inputdata( employ, workernumber) ; 
+            workernumber++ ;
+		}//end if
+	}//end while
+	showallsalary( employ, workernumber) ;
+	return 0 ;//
+}//End main
+
+void control( char incworker[2] ) {
+	printf("Do you want to Enter Employee Information? (y/n): ");
+	scanf("%s", incworker) ;
+}//end control
+
+void inputdata( worker employ[100], int workernumber) {
+	printf("Employee name: ") ;
+	getchar();
+	gets(employ[workernumber].name) ;
+	printf("Salary (Bath/Month): ") ;
+	scanf("%f", &employ[workernumber].salary) ; 
+	printf("Duration (year): ") ;
+	scanf("%d", &employ[workernumber].duration) ; 
+}//end inputdata
+
+void showallsalary( worker employ[100], int workernumber ) {
+	int i = 0 , j = 0 ; 
+	float sum = 0 ;
+	char highname[100] ; 
+	int highdur ;
+	float highsalary = -999999 ;
+	for( i=0 ; i < workernumber ; i++ ){
+		sum = sum + employ[i].salary ;
+	}//end while
+	printf( "---------------------------------------------\n") ;
+	printf( "Average of Salary : %.2f Bath\n", sum/workernumber) ;
+	printf( "Payment of every month : %.2f Bath\n" , sum) ;
+	for( j=0 ; j < workernumber; j++ ){ 
+		if ( employ[j].salary > highsalary ) {
+			highsalary = employ[j].salary ;
+			highdur = employ[j].duration ;
+			strcpy( highname, employ[j].name ) ;
+		}//end if
+		 
+	}// end while
+	printf( "**Most Salary in this business **\n" );
+	printf( "name : %s (%d Years)\n", highname, highdur) ; 
+	printf( "Salary : %.2f Bath", highsalary ) ; 
+}
